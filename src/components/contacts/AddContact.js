2d4f9f7b1bf0661d63ab9179eef6e08eae1bca67
pitbull90 <henrycordero@live.com>
2019-07-14 12:36:33 -0700

    Controlled Components and onChange event listener

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -7,12 +7,20 @@ class AddContact extends React.Component {
     phone: ""
   };
 
+  onSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
   render() {
+    const { name, email, phone } = this.state;
     return (
       <div className="card mb-3">
         <div className="card-header">Add Contact</div>
         <div className="card-body">
-          <form>
+          <form onSubmit={this.onSubmit}>
             <div className="form-group">
               <label htmlFor="name">Name</label>
               <input
@@ -20,6 +28,8 @@ class AddContact extends React.Component {
                 name="name"
                 className="form-control form-control-lg"
                 placeholder="Enter Name..."
+                value={name}
+                onChange={this.onChange}
               />
             </div>
 
@@ -30,6 +40,8 @@ class AddContact extends React.Component {
                 name="email"
                 className="form-control form-control-lg"
                 placeholder="Enter Email..."
+                value={email}
+                onChange={this.onChange}
               />
             </div>
 
@@ -40,6 +52,8 @@ class AddContact extends React.Component {
                 name="phone"
                 className="form-control form-control-lg"
                 placeholder="Enter Phone..."
+                value={phone}
+                onChange={this.onChange}
               />
             </div>
 
